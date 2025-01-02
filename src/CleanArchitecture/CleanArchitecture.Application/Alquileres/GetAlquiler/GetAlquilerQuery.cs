45777ede9de816b61a9f2b49180c534c6e151263
Felix Miranda <felixmiranda.net@gmail.com>
2025-01-01 22:58:37 -0500

    Add Dapper extension and create a query in SQL for GetAlquiler

---
+++ src/CleanArchitecture/CleanArchitecture.Application/Alquileres/GetAlquiler/GetAlquilerQuery.cs
@@ -0,0 +1,6 @@
+using System;
+using CleanArchitecture.Application.Abstractions.Messaging;
+
+namespace CleanArchitecture.Application.Alquileres.GetAlquiler;
+
+public sealed record GetAlquilerQuery(Guid AlquilerId) : IQuery<AlquilerResponse>;
