1ad100a599ebb00e869f3e2455c93e2225bff8db
Felix Miranda <felixmiranda.net@gmail.com>
2025-01-19 22:14:33 -0500

    Implement Unit Of Work and register all Domain Events

--- src/CleanArchitecture/CleanArchitecture.Infrastructure/ApplicationDbContext.cs
+++ src/CleanArchitecture/CleanArchitecture.Infrastructure/ApplicationDbContext.cs
@@ -1,21 +1,49 @@
-using System;
 using CleanArchitecture.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace CleanArchitecture.Infrastructure;
 
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
-    public ApplicationDbContext(DbContextOptions options) : base(options)
+    private readonly IPublisher _publisher;
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher) : base(options)
     {
-
+        _publisher = publisher;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(
+        CancellationToken cancellationToken = default
+    )
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventAsync();
+        return result;
+    }
+
+    private async Task PublishDomainEventAsync()
+    {
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                var domainEvents = entity.GetDomainEvents();
+                entity.ClearDomainEvents();
+                return domainEvents;
+            }).ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
     }
 
 }
--- src/CleanArchitecture/CleanArchitecture.Infrastructure/Repositories/UserRepository.cs
+++ src/CleanArchitecture/CleanArchitecture.Infrastructure/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
 using System;
+using CleanArchitecture.Domain.Users;
 
 namespace CleanArchitecture.Infrastructure.Repositories;
 
