126a3676729aca66696470889705719b340d93f3
Felix Miranda <felixmiranda.net@gmail.com>
2025-01-19 23:18:16 -0500

    Add concurrency and version in VehiculoConfiguration

--- src/CleanArchitecture/CleanArchitecture.Application/Alquileres/ReservarAlquiler/ReservarAlquilerCommandHandler.cs
+++ src/CleanArchitecture/CleanArchitecture.Application/Alquileres/ReservarAlquiler/ReservarAlquilerCommandHandler.cs
@@ -1,6 +1,6 @@
 using CleanArchitecture.Application.Abstractions.Clock;
 using CleanArchitecture.Application.Abstractions.Messaging;
-
+using CleanArchitecture.Application.Exceptions;
 using CleanArchitecture.Domain.Abstractions;
 using CleanArchitecture.Domain.Alquileres;
 using CleanArchitecture.Domain.Users;
@@ -53,20 +53,27 @@ internal sealed class ReservarAlquilerCommandHandler : ICommandHandler<ReservarA
         {
             return Result.Failure<Guid>(AlquilerErrors.Overlap);
         }
+        try
+        {
+            var alquiler = Alquiler.Reservar(
+                vehiculo,
+                user.Id,
+                duracion,
+                _dateTimeProvider.currentTime,
+                _precioService
+            );
 
-        var alquiler = Alquiler.Reservar(
-            vehiculo,
-            user.Id,
-            duracion,
-            _dateTimeProvider.currentTime,
-            _precioService
-        );
+            _alquilerRepository.Add(alquiler);
 
-        _alquilerRepository.Add(alquiler);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
 
-        await _unitOfWork.SaveChangesAsync(cancellationToken);
+            return alquiler.Id;
 
-        return alquiler.Id;
+        }
+        catch (ConcurrencyException)
+        {
+            return Result.Failure<Guid>(AlquilerErrors.Overlap);
+        }
 
     }
 }
---
+++ src/CleanArchitecture/CleanArchitecture.Application/Exceptions/ConcurrencyException.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CleanArchitecture.Application.Exceptions;
+
+public sealed class ConcurrencyException : Exception
+{
+    public ConcurrencyException(string message, Exception innerException) : base(message, innerException)
+    {
+
+    }
+}
